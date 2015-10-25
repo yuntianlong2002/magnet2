b5824020b87be9dadd77a5fdec1dc8862b79d267
Tianlong Yun <tyun@bear.cs.dartmouth.edu>
2015-10-25 18:01:09 -0400

    changed to another one page template

---
+++ test/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+    $(".buddy").on("swiperight",function(){
+	$(this).addClass('rotate-left').delay(700).fadeOut(1);
+	$('.buddy').find('.status').remove();
+
+	$(this).append('<div class="status like">Like!</div>');
+	if ( $(this).is(':last-child') ) {
+	    $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+	} else {
+	    $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+	}
+    });
+
+    $(".buddy").on("swipeleft",function(){
+	$(this).addClass('rotate-right').delay(700).fadeOut(1);
+	$('.buddy').find('.status').remove();
+	$(this).append('<div class="status dislike">Dislike!</div>');
+
+	if ( $(this).is(':last-child') ) {
+	    $('.buddy:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+	    alert('Na-na!');
+	} else {
+	    $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+	}
+    });
+
+});
+
+function initialize() {
+    var mapProp = {
+	center:new google.maps.LatLng(51.508742,-0.120850),
+	zoom:5,
+	mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
